5d5f26a88255710f5a72965a6f42606420fdc28f
Liedson Mendes <mendesliedson978@gmail.com>
2024-07-17 13:58:36 -0300

    Commit the exercises

---
+++ exercicies-ada-tech/src/main/java/com/ada/exercicies_ada_tech/exercicio/DayWeek.java
@@ -0,0 +1,27 @@
+package com.ada.exercicies_ada_tech.exercicio;
+
+import java.util.Scanner;
+
+public class DayWeek {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 7:");
+        int dia = scanner.nextInt();
+
+        String diaDaSemana = (dia == 1) ? "Domingo" :
+                             (dia == 2) ? "Segunda-feira" :
+                             (dia == 3) ? "Terça-feira" :
+                             (dia == 4) ? "Quarta-feira" :
+                             (dia == 5) ? "Quinta-feira" :
+                             (dia == 6) ? "Sexta-feira" :
+                             (dia == 7) ? "Sábado" :
+                             "Número inválido";
+
+        System.out.println("O dia semana corresponde é: " + diaDaSemana);
+
+        scanner.close();
+    }
+}
